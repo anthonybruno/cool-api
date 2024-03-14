87bd389ea03065ba65ae5c3b9a2260cbb0ffaabb
Anthony Bruno <anthonybruno@gmail.com>
2024-03-14 17:18:45 -0600

    Support for cors

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response, Application } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 2222;
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello world');
